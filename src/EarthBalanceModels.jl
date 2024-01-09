b813a8b67fe29dff585b4d9cb144e657ebec5abe
dknatan <natandominko@gmail.com>
2024-01-09 15:49:07 +0100

    edit tests

--- src/EarthBalanceModels.jl
+++ src/EarthBalanceModels.jl
@@ -2,22 +2,22 @@ module EarthBalanceModels
 
 function zero_dim_ebm_naive(u,p,t)
     S₀, σ, albedo = p
-    return (1 - albedo) / 4 * S₀ - σ * u[1]^4
+    return (1 - albedo)*(S₀/4) - σ * u[1]^4
 end
 
 function zero_dim_ebm_greenhouse(u,p,t)
     S₀, σ, albedo, ϵ = p
-    return (1 - albedo) / 4 * S₀ - ϵ * σ * u[1]^4
+    return (1 - albedo)*(S₀/4) - ϵ * σ * u[1]^4
 end
 
 function zero_dim_ebm_budyko(u,p,t)
     S₀, albedo, A, B = p
-    return (1 - albedo) / 4 * S₀ - A - B * u[1]
+    return (1 - albedo)*(S₀/4) - A - B * u[1]
 end
 
 function zero_dim_ebm_t_dep_albedo(du,u,p,t)
     S₀, σ, albedo, ϵ, α₀, α₁, T₀ = p
-    return (1 - albedo(u,p,t)) / 4 * S₀ - ϵ * σ * u[1]^4
+    return (1 - albedo(u,p,t))*(S₀/4) - ϵ * σ * u[1]^4
 end
 
 function albedo(u,p,t)
--- test/runtests.jl
+++ test/runtests.jl
@@ -4,13 +4,14 @@ using Test
 @testset "EarthBalanceModels.jl stable point tests" begin
     # Write your tests here.
     p1 = [1363., 5.67e-8, 0.3]
-    Tstar1 = ((1 - 0.3) * 1363 / 4 / 5.67e-8)^0.25
-    @test zero_dim_ebm_naive(Tstar1,p1,0.) ≈ 0.
-    p2 = [1363., 5.67e-8, 0.3, 0.6]
-    Tstar2 = ((1 - 0.3) * 1363 / 4 / 5.67e-8 / 0.6)^0.25
-    @test zero_dim_ebm_greenhouse(Tstar2,p2,0.) ≈ 0.
-    p3 = [1363., 0.3, 202., 1.45]
-    Tstar3 = ((1 - 0.3) * 1363 / 4 - 202.) / 1.45
-    @test zero_dim_ebm_budyko(Tstar3,p3,0.) ≈ 0.
+    Tstar1 = ((1 - 0.3) * (1363. / 4) / (5.67e-8))^0.25
+    print(Tstar1)
+    @test EarthBalanceModels.zero_dim_ebm_naive(Tstar1,p1,0.) ≈ 0.
+    # p2 = [1363., 5.67e-8, 0.3, 0.6]
+    # Tstar2 = ((1 - 0.3) * 1363 / 4 / 5.67e-8 / 0.6)^0.25
+    # @test EarthBalanceModels.zero_dim_ebm_greenhouse(Tstar2,p2,0.) ≈ 0.
+    # p3 = [1363., 0.3, 202., 1.45]
+    # Tstar3 = ((1 - 0.3) * 1363 / 4 - 202.) / 1.45
+    # @test EarthBalanceModels.zero_dim_ebm_budyko(Tstar3,p3,0.) ≈ 0.
 
 end
